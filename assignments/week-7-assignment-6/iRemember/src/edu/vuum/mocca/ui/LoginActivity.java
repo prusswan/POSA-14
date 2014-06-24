4750df89a2f9739ba55f6af1d70b369468b61f31
prusswan <prusswan@gmail.com>
2014-06-24 16:00:03 +0800

    completed assignment 6

--- assignments/week-7-assignment-6/iRemember/src/edu/vuum/mocca/ui/LoginActivity.java
+++ assignments/week-7-assignment-6/iRemember/src/edu/vuum/mocca/ui/LoginActivity.java
@@ -47,7 +47,7 @@ public class LoginActivity extends StoryActivityBase{
 	public static File getLoginFile (Context context) {
 		return StorageUtilities.getOutputMediaFile(context, 	// Line 48 vulnerability
 				StorageUtilities.MEDIA_TYPE_TEXT, 
-				MAX_SECURITY, 
+				StorageUtilities.SECURITY_PRIVATE, // sensitive info should be kept in private area
 				"login.txt");
 	}
 	
