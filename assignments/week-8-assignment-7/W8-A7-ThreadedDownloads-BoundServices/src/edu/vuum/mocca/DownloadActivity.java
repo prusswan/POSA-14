5ceda5d3810beecc4ef074162011276a42fd78bf
prusswan <prusswan@gmail.com>
2014-06-30 03:54:02 +0800

    async button and DownloadCallback tests failing

--- assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -10,38 +10,35 @@ import android.view.View;
 
 /**
  * This is the main Activity that the program uses to start the
- * ThreadedDownloads application.  It allows the user to input the URL
- * of an image and download that image using one of two different
- * Android Bound Service implementations: synchronous and
- * asynchronous.  The Activity starts the Service using bindService().
- * After the Service is started, its onBind() hook method returns an
- * implementation of an AIDL interface to the Activity by
- * asynchronously calling the onServiceConnected() hook method in the
- * Activity.  The AIDL interface object that's returned can then be
- * used to interact with the Service either synchronously or
- * asynchronously, depending on the type of AIDL interface requested.
+ * ThreadedDownloads application. It allows the user to input the URL of an
+ * image and download that image using one of two different Android Bound
+ * Service implementations: synchronous and asynchronous. The Activity starts
+ * the Service using bindService(). After the Service is started, its onBind()
+ * hook method returns an implementation of an AIDL interface to the Activity by
+ * asynchronously calling the onServiceConnected() hook method in the Activity.
+ * The AIDL interface object that's returned can then be used to interact with
+ * the Service either synchronously or asynchronously, depending on the type of
+ * AIDL interface requested.
  * 
- * Starting Bound Services to run synchronously in background Threads
- * from the asynchronous UI Thread is an example of the
- * Half-Sync/Half-Async Pattern.  Starting Bound Services using
- * Intents is an example of the Activator and Command Processor
- * patterns.  The DownloadActivity plays the role of the Creator and
- * creates a Command in the form of an Intent.  The Intent is received
- * by the Service process, which plays the role of the Executor.
+ * Starting Bound Services to run synchronously in background Threads from the
+ * asynchronous UI Thread is an example of the Half-Sync/Half-Async Pattern.
+ * Starting Bound Services using Intents is an example of the Activator and
+ * Command Processor patterns. The DownloadActivity plays the role of the
+ * Creator and creates a Command in the form of an Intent. The Intent is
+ * received by the Service process, which plays the role of the Executor.
  * 
- * The use of AIDL interfaces to pass information between two
- * different processes is an example of the Broker Pattern, in which
- * all communication-related functionality is encapsulated in the AIDL
- * interface and the underlying Android Binder framework, shielding
- * applications from tedious and error-prone aspects of inter-process
- * communication.
+ * The use of AIDL interfaces to pass information between two different
+ * processes is an example of the Broker Pattern, in which all
+ * communication-related functionality is encapsulated in the AIDL interface and
+ * the underlying Android Binder framework, shielding applications from tedious
+ * and error-prone aspects of inter-process communication.
  */
 public class DownloadActivity extends DownloadBase {
     /**
      * Used for debugging.
      */
-    private final String TAG = this.getClass().getSimpleName(); 
-    
+    private final String TAG = this.getClass().getSimpleName();
+
     /**
      * The AIDL Interface that's used to make twoway calls to the
      * DownloadServiceSync Service.  This object plays the role of
@@ -49,7 +46,7 @@ public class DownloadActivity extends DownloadBase {
      * connection to the Service.
      */
     DownloadCall mDownloadCall;
-     
+
     /**
      * The AIDL Interface that we will use to make oneway calls to the
      * DownloadServiceAsync Service.  This plays the role of Requestor
@@ -57,10 +54,10 @@ public class DownloadActivity extends DownloadBase {
      * to the Service.
      */
     DownloadRequest mDownloadRequest;
-     
-    /** 
-     * This ServiceConnection is used to receive results after binding
-     * to the DownloadServiceSync Service using bindService().
+
+    /**
+     * This ServiceConnection is used to receive results after binding to the
+     * DownloadServiceSync Service using bindService().
      */
     ServiceConnection mServiceConnectionSync = new ServiceConnection() {
             /**
@@ -70,13 +67,13 @@ public class DownloadActivity extends DownloadBase {
              */
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
-            	Log.d(TAG, "ComponentName: " + name);
+            Log.d(TAG, "ComponentName: " + name);
                 // TODO You fill in here to replace null with a call
                 // to a generated stub method that converts the
                 // service parameter into an interface that can be
                 // used to make RPC calls to the Service.
 
-                mDownloadCall = null;
+            mDownloadCall = DownloadCall.Stub.asInterface(service);
             }
 
             /**
@@ -88,12 +85,12 @@ public class DownloadActivity extends DownloadBase {
             public void onServiceDisconnected(ComponentName name) {
                 mDownloadCall = null;
             }
-    	 
+
         };
-     
-    /** 
-     * This ServiceConnection is used to receive results after binding
-     * to the DownloadServiceAsync Service using bindService().
+
+    /**
+     * This ServiceConnection is used to receive results after binding to the
+     * DownloadServiceAsync Service using bindService().
      */
     ServiceConnection mServiceConnectionAsync = new ServiceConnection() {
             /**
@@ -102,14 +99,14 @@ public class DownloadActivity extends DownloadBase {
              * mDownloadRequest.
              */
             @Override
-		public void onServiceConnected(ComponentName name,
+        public void onServiceConnected(ComponentName name,
                                                IBinder service) {
                 // TODO You fill in here to replace null with a call
                 // to a generated stub method that converts the
                 // service parameter into an interface that can be
                 // used to make RPC calls to the Service.
 
-                mDownloadRequest = null;
+            mDownloadRequest = DownloadRequest.Stub.asInterface(service);
             }
 
             /**
@@ -118,18 +115,18 @@ public class DownloadActivity extends DownloadBase {
              * but the service will not respond to any requests.
              */
             @Override
-		public void onServiceDisconnected(ComponentName name) {
+        public void onServiceDisconnected(ComponentName name) {
                 mDownloadRequest = null;
             }
         };
-     
+
     /**
-     * The implementation of the DownloadCallback AIDL
-     * Interface. Should be passed to the DownloadBoundServiceAsync
-     * Service using the DownloadRequest.downloadImage() method.
+     * The implementation of the DownloadCallback AIDL Interface. Should be
+     * passed to the DownloadBoundServiceAsync Service using the
+     * DownloadRequest.downloadImage() method.
      * 
-     * This implementation of DownloadCallback.Stub plays the role of
-     * Invoker in the Broker Pattern.
+     * This implementation of DownloadCallback.Stub plays the role of Invoker in
+     * the Broker Pattern.
      */
     DownloadCallback.Stub mDownloadCallback = new DownloadCallback.Stub() {
             /**
@@ -145,29 +142,40 @@ public class DownloadActivity extends DownloadBase {
                 // sendPath().  Please use displayBitmap() defined in
                 // DownloadBase.
 
-                Runnable displayRunnable = null;
+            Runnable displayRunnable = new Runnable() {
+
+                @Override
+                public void run() {
+                    displayBitmap(imagePathname);
+                }
+            };
             }
         };
-     
+
     /**
      * This method is called when a user presses a button (see
      * res/layout/activity_download.xml)
+     * 
+     * @throws RemoteException
      */
-    public void runService(View view) {
+    public void runService(View view) throws RemoteException {
         Uri uri = Uri.parse(getUrlString());
 
         hideKeyboard();
 
-    	switch (view.getId()) {
+        switch (view.getId()) {
         case R.id.bound_sync_button:
             // TODO - You fill in here to use mDownloadCall to
             // download the image & then display it.
+            String pathname = mDownloadCall.downloadImage(uri);
+            displayBitmap(pathname);
             break;
 
         case R.id.bound_async_button:
             // TODO - You fill in here to call downloadImage() on
             // mDownloadRequest, passing in the appropriate Uri and
             // callback.
+            mDownloadRequest.downloadImage(uri, mDownloadCallback);
             break;
         }
     }
@@ -178,59 +186,59 @@ public class DownloadActivity extends DownloadBase {
      */
     @Override
     public void onStart () {
-    	super.onStart();
-    	
-    	// Bind this activity to the DownloadBoundService* Services if
-    	// they aren't already bound Use mBoundSync/mBoundAsync
-    	if (mDownloadCall == null) 
-            bindService(DownloadBoundServiceSync.makeIntent(this), 
-                        mServiceConnectionSync, 
+        super.onStart();
+
+        // Bind this activity to the DownloadBoundService* Services if
+        // they aren't already bound Use mBoundSync/mBoundAsync
+        if (mDownloadCall == null)
+            bindService(DownloadBoundServiceSync.makeIntent(this),
+                mServiceConnectionSync,
                         BIND_AUTO_CREATE);
-    	if (mDownloadRequest == null)
-            bindService(DownloadBoundServiceAsync.makeIntent(this), 
-                        mServiceConnectionAsync, 
+        if (mDownloadRequest == null)
+            bindService(DownloadBoundServiceAsync.makeIntent(this),
+                mServiceConnectionAsync,
                         BIND_AUTO_CREATE);
     }
-    
+
     /**
      * Hook method called when the DownloadActivity becomes completely
      * hidden to unbind the Activity from the Services.
      */
     @Override
     public void onStop () {
-    	super.onStop();
-    	
-    	// Unbind the Sync/Async Services if they are bound. Use
-    	// mBoundSync/mBoundAsync
-    	if (mDownloadCall != null) 
+        super.onStop();
+
+        // Unbind the Sync/Async Services if they are bound. Use
+        // mBoundSync/mBoundAsync
+        if (mDownloadCall != null)
             unbindService(mServiceConnectionSync);
-    	if (mDownloadRequest != null) 
+        if (mDownloadRequest != null)
             unbindService(mServiceConnectionAsync);
     }
-    
+
     // Public accessor method for testing purposes
     public DownloadCall getDownloadCall () {
-    	return mDownloadCall;
+        return mDownloadCall;
     }
-    
+
     // Public accessor method for testing purposes
     public DownloadRequest getDownloadRequest () {
-    	return mDownloadRequest;
+        return mDownloadRequest;
     }
-    
+
     // Public accessor method for testing purposes
     public DownloadCallback getDownloadCallback () {
-    	return mDownloadCallback;
+        return mDownloadCallback;
     }
-    
+
     // Public accessor method for testing purposes
     public boolean isBoundToSync () {
-    	return mDownloadCall != null;
+        return mDownloadCall != null;
     }
-    
+
     // Public accessor method for testing purposes
     public boolean isBoundToAsync () {
-    	return mDownloadRequest != null;
-    }     
-    
+        return mDownloadRequest != null;
+    }
+
 }
--- assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadBoundServiceAsync.java
+++ assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadBoundServiceAsync.java
@@ -9,41 +9,36 @@ import android.os.RemoteException;
 
 /**
  * @class DownloadBoundServiceAsync
- 
- * @brief This class handles downloads using asynchronous AIDL
- *        interactions.  The component that binds to this service
- *        should receive an IBinder. This IBinder should be an
- *        instance of DownloadRequest, which extends IBinder. The
- *        component can then interact with this service by making
- *        normal calls on the DownloadRequest object. Specifically,
- *        the component can ask this service to download an image,
- *        passing in a DownloadCallback object.  Once the download is
- *        finished, this service should send the pathname of the
- *        downloaded file back to the calling component by calling
- *        sendPath() on the DownloadCallback object.
- *  
- *        AIDL is an example of the Broker Pattern, in which all
- *        interprocess communication details are hidden behind the
- *        AIDL interfaces.
+ * 
+ * @brief This class handles downloads using asynchronous AIDL interactions. The
+ *        component that binds to this service should receive an IBinder. This
+ *        IBinder should be an instance of DownloadRequest, which extends
+ *        IBinder. The component can then interact with this service by making
+ *        normal calls on the DownloadRequest object. Specifically, the
+ *        component can ask this service to download an image, passing in a
+ *        DownloadCallback object. Once the download is finished, this service
+ *        should send the pathname of the downloaded file back to the calling
+ *        component by calling sendPath() on the DownloadCallback object.
+ * 
+ *        AIDL is an example of the Broker Pattern, in which all interprocess
+ *        communication details are hidden behind the AIDL interfaces.
  */
 public class DownloadBoundServiceAsync extends Service{
     /**
-     * The concrete implementation of the AIDL Interface
-     * DownloadRequest.  We extend the Stub class, which implements
-     * DownloadRequest, so that Android can properly handle calls
-     * across process boundaries.
+     * The concrete implementation of the AIDL Interface DownloadRequest. We
+     * extend the Stub class, which implements DownloadRequest, so that Android
+     * can properly handle calls across process boundaries.
      * 
-     * This implementation plays the role of Invoker in the Broker
-     * Pattern.
+     * This implementation plays the role of Invoker in the Broker Pattern.
      */
     DownloadRequest.Stub mDownloadRequestImpl = new DownloadRequest.Stub() {
             /**
-             * Download the image at the given Uri and return a
-             * pathname to the file on the Android file system by
-             * calling the sendPath() method on the provided callback
-             * 
-             * Use the methods defined in DownloadUtils for code brevity.
-             */
+         * Download the image at the given Uri and return a pathname to the file
+         * on the Android file system by calling the sendPath() method on the
+         * provided callback
+         * 
+         * Use the methods defined in DownloadUtils for code brevity.
+         */
             @Override
             public void downloadImage(Uri uri,
                                       DownloadCallback callback)
@@ -52,10 +47,13 @@ public class DownloadBoundServiceAsync extends Service{
                 // the appropriate helper method in DownloadUtils and
                 // then send the pathname back to the client via the
                 // callback object.
+            String pathname = DownloadUtils.downloadFile(
+                DownloadBoundServiceAsync.this, uri);
+            callback.sendPath(pathname);
             }
-		
-	};
-	
+
+    };
+
     /**
      * Called when a component calls bindService() with the proper
      * intent.  Return the concrete implementation of DownloadRequest
@@ -75,6 +73,7 @@ public class DownloadBoundServiceAsync extends Service{
     public static Intent makeIntent(Context context) {
         // TODO - replace the null to create the appropriate Intent
         // and return it to the caller.
-        return null;
+        Intent intent = new Intent(context, DownloadBoundServiceAsync.class);
+        return intent;
     }
 }
